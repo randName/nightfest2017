afc2607e9ff42153bbe2618010e8e0fd67618b72
Shun Git <kwokshungit@gmail.com>
2017-07-11 09:07:41 +0800

    fix fps mode

--- js/fps.js
+++ js/fps.js
@@ -2,12 +2,14 @@ var FPS = {
     plElem: [ 'pointer', 'mozPointer', 'webkitPointer' ],
     controls: null,
     camera: null,
+    keys: { 65: 0, 87: 1, 68: 2, 83: 3, 81: 4, 69: 5 },
     prevTime: performance.now(),
     velocity: new THREE.Vector3(),
-    movement: [ false, false, false, false ],
+    movement: [ false, false, false, false, false, false ],
     init: function(camera) {
         var self = this;
         var info = document.getElementById('info');
+        var fpsbtn = document.getElementById('fpsbtn');
 
         if ( this.plElem.some(function(e){ return e+'LockElement' in document }) ) {
             var db = document.body;
@@ -22,15 +24,10 @@ var FPS = {
             var handleKey = function(e) {
                 var kc = e.keyCode, kd = ( e.type == "keydown" );
                 if ( ! self.controls.enabled ) {
-                    if ( kc == 70 && !kd ) db.requestPointerLock();
                     return;
                 }
-
-                var wasd = { 65: 0, 87: 1, 68: 2, 83: 3 };
-                if ( kc > 36 && kc < 41 ) {
-                    self.movement[kc-37] = kd;
-                } else if ( kc in wasd ) {
-                    self.movement[wasd[kc]] = kd;
+                if ( kc in self.keys ) {
+                    self.movement[self.keys[kc]] = kd;
                 }
             }
 
@@ -40,9 +37,9 @@ var FPS = {
                 document.addEventListener(e.toLowerCase()+'lockchange', pointerlockchange, false);
                 document.addEventListener(e.toLowerCase()+'lockerror', pointerlockerror, false);
             });
-            info.innerHTML = "Press 'F' to enter FPS mode";
+            fpsbtn.onclick = function () { if ( ! self.controls.enabled ) db.requestPointerLock(); }
         } else {
-            info.innerHTML = "Your browser doesn't seem to support Pointer Lock API";
+            fpsbtn.style.display = "none";
         }
 
         this.controls = new THREE.PointerLockControls(camera);
@@ -52,14 +49,18 @@ var FPS = {
     update: function () {
 		if ( ! this.controls.enabled ) return;
         var time = performance.now();
-        var dt = ( time - this.prevTime ) / 100;
+        var dt = ( time - this.prevTime ) / 1000;
         this.velocity.x -= this.velocity.x * 10.0 * dt;
+        this.velocity.y -= this.velocity.y * 10.0 * dt;
         this.velocity.z -= this.velocity.z * 10.0 * dt;
-        if ( this.movement[0] ) this.velocity.x -= 300.0 * dt;
-        if ( this.movement[1] ) this.velocity.z -= 300.0 * dt;
-        if ( this.movement[2] ) this.velocity.x += 300.0 * dt;
-        if ( this.movement[3] ) this.velocity.z += 300.0 * dt;
+        if ( this.movement[0] ) this.velocity.x -= 400.0 * dt;
+        if ( this.movement[1] ) this.velocity.z -= 400.0 * dt;
+        if ( this.movement[2] ) this.velocity.x += 400.0 * dt;
+        if ( this.movement[3] ) this.velocity.z += 400.0 * dt;
+        if ( this.movement[4] ) this.velocity.y -= 400.0 * dt;
+        if ( this.movement[5] ) this.velocity.y += 400.0 * dt;
         this.camera.translateX( this.velocity.x * dt );
+        this.camera.translateY( this.velocity.y * dt );
         this.camera.translateZ( this.velocity.z * dt );
         this.prevTime = time;
     },
