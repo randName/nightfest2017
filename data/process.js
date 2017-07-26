4d5a370566bdd1b524e22a159077abd0da53f43b
Shun Git <kwokshungit@gmail.com>
2017-07-26 10:50:35 +0800

    streamline modules

--- data/process.js
+++ data/process.js
@@ -1,26 +1,22 @@
-var DATA = {
-    length: 0,
-    center: [0, 0, 0],
-    vertices: [],
-    original: [],
-    remap: function (p){
+var DATA = (function(){
+    var remap = function (p){
         p = p.map((x, i) => (x-this[i])*0.1, this);
         return [ p[0], p[2], -p[1] ];
-    },
-    init: function(){
-        this.lights = RAW_LIGHTS.reduce((t, r) => (t + r.length), 0);
-        this.center = this.center.map(function(n, i){
-            return RAW_LIGHTS.reduce((t, r) => t + r.reduce((s, c) => s + c[i], 0), 0)/this
-        }, this.lights);
-        var tmp = RAW_LIGHTS.map((r, i) => [i, r[r.length-1][0], r.map(this.remap, this.center)]);
-        tmp.sort((a, b) => (a[1] - b[1]));
-        this.vertices = tmp.map(r => r[2]);
-        this.original = tmp.map(r => r[0]);
-        this.length = RAW_LIGHTS.length;
     }
-}
 
-DATA.init();
+    var lights = RAW_LIGHTS.reduce((t, r) => (t + r.length), 0);
+    var center = [0, 1, 2].map(i => RAW_LIGHTS.reduce((t, r) => t + r.reduce((s, c) => s + c[i], 0), 0)/lights);
+    var tmp = RAW_LIGHTS.map((r, i) => [i, r[r.length-1][0], r.map(remap, center)]);
+    tmp.sort((a, b) => (a[1] - b[1]));
+
+    return {
+        center: center,
+        lights: lights,
+        length: RAW_LIGHTS.length,
+        vertices: tmp.map(r => r[2]),
+        original: tmp.map(r => r[0]),
+    }
+})();
 
 var EXAMPLE = {
     list: [],
--- js/app.js
+++ js/app.js
@@ -4,6 +4,24 @@
     var startpos = [ 625, 0, 125 ];
     var orbcontrols, orbcamera, fpscontrols, fpscamera;
 
+    function makefloor(){
+        var floor = new THREE.Mesh(
+            new THREE.PlaneGeometry(1600, 600),
+            new THREE.MeshPhongMaterial({color: 0x101010})
+        );
+        floor.rotation.x -= Math.PI/2;
+        floor.position.y -= 100;
+        return floor;
+    }
+
+    function makecamera(){
+        var fieldOfView = 75; // humans: 150:210
+        var aspectRatio = window.innerWidth/window.innerHeight;
+        var nearPlane = 1;
+        var farPlane = 2000;
+        return new THREE.PerspectiveCamera(fieldOfView, aspectRatio, nearPlane, farPlane);
+    }
+
     init();
     animate();
 
